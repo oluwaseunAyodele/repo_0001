c679b651c33f0f112d46faa8a57c772bd92f51a8
oluwaseunAyodele <44782724+oluwaseunAyodele@users.noreply.github.com>
2019-05-22 08:33:55 +0100

    Add files via upload

---
+++ do_view_box.php
@@ -0,0 +1,17 @@
+<?php
+//PRINT FOUR ITEMS ON A ROW 
+//THEN PRINT A BAR , THEN 
+//PRINT ANOTHER FOUR ITEMS
+
+	$imx="pics.jpg";
+	echo"<div style='width:25%;height:30%;border:solid lightgray 1px;padding:1em;margin:0.4em;float:left'>";
+	echo "<img src=".$imx." style='width:100%;height:110%;'/>";
+	echo "</div>";
+	
+
+
+
+?>
+
+
+
