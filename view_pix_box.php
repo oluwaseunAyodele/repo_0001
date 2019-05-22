542cc7af451cf58a1850af2f5a0c87e23d9d484d
oluwaseunAyodele <44782724+oluwaseunAyodele@users.noreply.github.com>
2019-05-22 08:57:53 +0100

    Add files via upload

---
+++ view_pix_box.php
@@ -0,0 +1,24 @@
+
+
+<head>
+<!-- Stylesheets -->
+<link rel=stylesheet   href="styling.css"   >
+
+
+<?php
+//PRINT FOUR ITEMS ON A ROW 
+//THEN PRINT A BAR , THEN 
+//PRINT ANOTHER FOUR ITEMS
+
+	$imx="pics.jpg";
+	echo"<div class=pix_box >";
+	echo "<img src=".$imx." class=pix_self />";
+	echo "</div>";
+	
+
+
+
+?>
+
+
+
